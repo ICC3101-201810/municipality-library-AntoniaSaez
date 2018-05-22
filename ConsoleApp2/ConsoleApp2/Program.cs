70dfb3ecd9a8b3fbba2b7eeb09e359be11c5a583
ING <fagomez1@miuandes.cl>
2018-05-22 17:55:03 -0400

    lab

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        [DllImport("ClassLibraryBueno")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        static void Main(string[] args)
+        {
+        }
+    }
+}
